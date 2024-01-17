19da3add87df88cf22ff060178834a327a77d0cf
YashSharma412 <sharmayashsatiish@gmail.com>
2024-01-17 13:39:48 +0530

    test1

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,13 @@
-
 import React from "react";
 import './../styles/App.css';
-
+import ConditionalWeather from "./ConditionalWeather";
 const App = () => {
   return (
-    <div>
+    <div id="main" className="main">
         {/* Do not remove the main div */}
+        <ConditionalWeather/>
     </div>
   )
 }
 
-export default App
+export default App;
---
+++ src/components/ConditionalWeather.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function ConditionalWeather() {
+  const Weather = {
+    temperature: 25,
+    condition: "Sunny",
+  };
+  const tempColor = Weather.temperature > 20 ? "red" : "blue";
+  return (
+    <div>
+      <h2>Weather Report</h2>
+      <p>
+        Temerature: 
+        <span style={{ color: tempColor }}> {Weather.temperature}</span>
+      </p>
+      <p>Conditions: {Weather.condition}</p>
+    </div>
+  );
+}
+
+export default ConditionalWeather;
